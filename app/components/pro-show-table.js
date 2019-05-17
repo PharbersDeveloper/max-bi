150ff28daf16ae0386707bdc25593b88007d8a82
wodelu <1058744843@qq.com>
2019-05-17 19:45:14 +0800

    提交

---
+++ app/components/pro-show-table.js
@@ -0,0 +1,20 @@
+import Component from '@ember/component';
+import { A } from '@ember/array';
+
+export default Component.extend({
+    files: A([{
+        brand: 1113527833907,
+        sales: 2221858559446,
+        market: 3333384442003,
+        ms: 444839547002,
+        growth: 5551119068034,
+        ei: 2262734746
+    },{
+        brand: 1113527833907,
+        sales: 2221858559446,
+        market: 3333384442003,
+        ms: 444839547002,
+        growth: 5551119068034,
+        ei: 2262734746
+    }]),
+});
