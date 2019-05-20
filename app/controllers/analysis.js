74cbe6512dff75c3444e3e957078eb95fd7d0b55
wodelu <1058744843@qq.com>
2019-05-20 15:30:17 +0800

    overview部分样式

--- app/controllers/analysis.js
+++ app/controllers/analysis.js
@@ -6,5 +6,24 @@ export default Controller.extend({
     tabs: A(['Overall Market', 'Regional Analysis', 'Province Analysis', 'City Analysis']),
     actions: {
         onTabClicked() {},
-    }
+    },
+    init() {
+		this._super(...arguments);
+		this.set('lineData', A([{
+			name: 'MNC',
+			date: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-05',
+				'2018-06', '2018-07', '2018-08',
+				'2018-09', '2018-10', '2018-11', '2018-12'],
+			data: [320, 332, 301, 334, 390, 330, 320, 255, 350, 337, 365, 912]
+		},
+		{
+			name: 'ELILILLY GROUP',
+			date: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-05',
+				'2018-06', '2018-07', '2018-08',
+				'2018-09', '2018-10', '2018-11', '2018-12'],
+			data: [820, 932, 901, 934, 1290, 1330, 1320, 244, 365, 109, 203, 273]
+		}]));
+		// this.set('lineColor',  A(['rgb(115,171,255)', 'rgb(255,227,128)', 'rgb(73,229,245)','rgb(52,246,188)', 'rgb(54,179,126)']));
+		// this.set('legendPosition', { left: 'center' });
+	}
 });
