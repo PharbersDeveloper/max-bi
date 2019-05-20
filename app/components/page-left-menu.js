40311cf2bb61ee0d375dc788ecb1fb94d1316148
Chenghao Sun <601330984@qq.com>
2019-05-20 17:38:42 +0800

    样式问题

--- app/components/page-left-menu.js
+++ app/components/page-left-menu.js
@@ -3,13 +3,6 @@ import { A } from '@ember/array';
 
 export default Component.extend({
     
-    keyValue: A([
-        { id: 1, value: '将进酒' },
-        { id: 2, value: '桃花源记' },
-        { id: 3, value: '滕王阁序' },
-        { id: 4, value: '林尽水源' },
-
-    ]),
     keyName: A([
         { id: 1, name: '将进酒' },
         { id: 2, name: '桃花源记' },
--- app/controllers/chc-analysis.js
+++ app/controllers/chc-analysis.js
@@ -4,7 +4,34 @@ import { A } from '@ember/array';
 export default Controller.extend({
     cur_tab_idx: 0,
     tabs: A(['Overall Market', 'Province Analysis', 'City Analysis']),
+    collapsed: false,
     actions: {
         onTabClicked() {},
-    }
+        toggle() {
+            if(this.collapsed) {
+                this.set('collapsed', false)
+            } else {
+                this.set('collapsed', true)
+            }
+        }
+    },
+    init() {
+		this._super(...arguments);
+		this.set('lineData', A([{
+			name: 'MNC',
+			date: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-05',
+				'2018-06', '2018-07', '2018-08',
+				'2018-09', '2018-10', '2018-11', '2018-12'],
+			data: [320, 332, 301, 334, 390, 330, 320, 255, 350, 337, 365, 912]
+		},
+		{
+			name: 'ELILILLY GROUP',
+			date: ['2018-01', '2018-02', '2018-03', '2018-04', '2018-05',
+				'2018-06', '2018-07', '2018-08',
+				'2018-09', '2018-10', '2018-11', '2018-12'],
+			data: [820, 932, 901, 934, 1290, 1330, 1320, 244, 365, 109, 203, 273]
+		}]));
+		// this.set('lineColor',  A(['rgb(115,171,255)', 'rgb(255,227,128)', 'rgb(73,229,245)','rgb(52,246,188)', 'rgb(54,179,126)']));
+		this.set('legendPosition', { x: 'center', y: 'center', });
+	}
 });
