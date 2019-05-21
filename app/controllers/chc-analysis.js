81a51b8252691f1564384679cf52a09dfe18ac53
Chenghao Sun <601330984@qq.com>
2019-05-21 19:19:57 +0800

    Auto stash before merge of "chsun-0515" and "origin/chsun-0515"

--- app/controllers/chc-analysis.js
+++ app/controllers/chc-analysis.js
@@ -4,19 +4,28 @@ import { computed } from '@ember/object'
 
 export default Controller.extend({
     cur_tab_idx: 0,
-    tabs: A(['Overall Market', 'City Analysis']),
+	tabs: A(['Overall Market', 'City Analysis']),
 	collapsed: false,
 	
 	curMarket: null,
-	overallInfoDone: false,
+	curInfoId: '',
+	
+	xAxisDataHeader: A([]),
+	doubleData: A([]),
 
 	overallInfo: computed('curMarket', function() {
 		let result = this.store.queryRecord('overallInfo', {'market-id': this.curMarket.id, 'orderby': ''});
+		result.then(res => {
+			this.set('curInfoId', res.id);
+		})
         return result;
 	}),
 	
-	sampleCover: computed('overallInfo', function() {
-		let result = this.store.query('sampleCover', {'info-id': this.overallInfo.id});
+	sampleCover: computed('curInfoId', function() {
+		let result = this.store.query('sampleCover', {'info-id': this.curInfoId});
+		result.then(() => {
+			this.setDoubleData();
+		})
         return result;
     }),
 
@@ -29,7 +38,8 @@ export default Controller.extend({
                 this.set('collapsed', true)
             }
         }
-    },
+	},
+	
     init() {
 		this._super(...arguments);
 		this.set('lineData', A([{
@@ -48,5 +58,39 @@ export default Controller.extend({
 		}]));
 		// this.set('lineColor',  A(['rgb(115,171,255)', 'rgb(255,227,128)', 'rgb(73,229,245)','rgb(52,246,188)', 'rgb(54,179,126)']));
 		this.set('legendPosition', { x: 'center', y: 'center', });
+	},
+
+	
+	setDoubleData() {
+		// window.console.log();
+		let xAxisData = A([]);
+		let resultArr = A([{
+			name: 'Universe',
+			type: 'bar',
+			yAxisIndex: 1,
+			data: []
+		},
+		{
+			name: 'Sample',
+			type: 'bar',
+			yAxisIndex: 1,
+			data: []
+		},
+		{
+			name: 'Coverage Ratio',
+			type: 'line',
+			data: []
+		}]);
+
+		this.sampleCover.forEach(elem => {
+			xAxisData.push(elem.cityName)
+			resultArr[0].data.push(elem.universeNum);
+			resultArr[1].data.push(elem.sampleNum);
+			resultArr[2].data.push(elem.coverageRatio);
+		});
+		window.console.log(xAxisData);
+		window.console.log(resultArr);
+		this.set('xAxisDataHeader', xAxisData)
+		this.set('doubleData', resultArr)
 	}
 });
