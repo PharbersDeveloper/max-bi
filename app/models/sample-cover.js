e759cbb6d1047827ec70e7695cc2a002c6524a02
Chenghao Sun <601330984@qq.com>
2019-05-21 19:45:56 +0800

    小修改

--- app/models/sample-cover.js
+++ app/models/sample-cover.js
@@ -2,10 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     infoId: DS.attr('string'),
-    cityName: DS.attr('string'),
+    cityId: DS.attr('string'),
+    
     privateHospitalNum: DS.attr('number'),
     universeNum: DS.attr('number'),
     sampleNum: DS.attr('number'),
     coverageRatio: DS.attr(),
 
+    city: DS.belongsTo('city'),
 });
