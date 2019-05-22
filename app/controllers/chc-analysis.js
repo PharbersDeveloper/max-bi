4234b3ac14bc214b52734d60e106eaf8f60c9427
Chenghao Sun <601330984@qq.com>
2019-05-22 20:59:29 +0800

    第一页折线图结束

--- app/controllers/chc-analysis.js
+++ app/controllers/chc-analysis.js
@@ -21,6 +21,7 @@ export default Controller.extend({
 	doubleData: A([]),
 	productTableListData: A([]),
 	totalProObj: null,
+	overallLineData: A([]),
 
 	overallInfo: computed('curMarket', function() {
 		let result = this.store.queryRecord('overallInfo', {'market-id': this.curMarket.id, 'orderby': ''});
@@ -109,7 +110,7 @@ export default Controller.extend({
 		result.then(res => {
 			let tempRecords = A([]);
 			let finalArr = A([]);
-			res.forEach((elem, index) => {
+			res.forEach((elem, index, arr) => {
 				let tempObj = {};
 				tempObj.brand = elem.product.get('title');
 				tempRecords[index] = this.store.query('salesRecord', {
@@ -138,19 +139,86 @@ export default Controller.extend({
 						}
 					})
 					finalArr.pushObject(tempObj)
-					// window.console.log(finalArr);
-					this.set('productTableListData', finalArr);
-					let totalPro = finalArr.reduce((total, ele) =>  {
-						return {
-							totalSales: Number(total.totalSales)+Number(ele.sales),
-							totalMarketShare:  Number(total.totalMarketShare)+ Number(ele.marketShare),
-							totalMs: total.totalMs+ele.ms,
-							totalGrowth: total.totalGrowth+ele.growth,
-							totalEi: total.totalEi+ele.ei
-						}
-					}, {totalSales:0, totalMarketShare: 0, totalMs: 0, totalGrowth: 0, totalEi: 0});
-					// console.log(totalPro)
-					this.set('totalProObj', totalPro)
+					if(finalArr.length == arr.length) {
+						// window.console.log(finalArr);
+						this.set('productTableListData', finalArr);
+						let totalPro = finalArr.reduce((total, ele) =>  {
+							return {
+								totalSales: Number(total.totalSales)+Number(ele.sales),
+								totalMarketShare:  Number(total.totalMarketShare)+ Number(ele.marketShare),
+								totalMs: total.totalMs+ele.ms,
+								totalGrowth: total.totalGrowth+ele.growth,
+								totalEi: total.totalEi+ele.ei
+							}
+						}, {totalSales:0, totalMarketShare: 0, totalMs: 0, totalGrowth: 0, totalEi: 0});
+						// console.log(totalPro)
+						this.set('totalProObj', totalPro)
+					}
+				})
+			})
+		})
+        return result;
+	}),
+
+	overallLineList: computed('curInfoId', 'cur_tab_idx', 'curDate', 'dateDataStateFlag', function() {
+		if (this.curDate == undefined || this.curDate == null) {
+			return;
+		} 
+		let result = this.store.query('salesRecord', {
+			'info-id': this.curInfoId, 
+			'date': this.curDate.date,
+			'date-type': this.curDateType,
+			'address-type': this.cur_tab_idx,
+			'goods-type': 1,
+			'value-type': 1,
+			'orderby': '-VALUE',
+			'skip': 0,
+			'take': this.tableTake,
+		});
+		result.then(res => {
+			let tempRecords = A([]);
+			let finalArr = A([]);
+			res.forEach((elem, index, arr) => {
+				let tempObj = {};
+				let tempArrX = A([]);
+				let tempArrY = A([]);
+				tempObj.name = elem.product.get('title');
+				tempRecords[index] = this.store.query('salesRecord', {
+					'info-id': this.curInfoId, 
+					// 'date': this.curDate.date,
+					'date-type': 2,
+					'address-type': this.cur_tab_idx,
+					'goods-type': 1,
+					'goods-id': elem.goodsId,
+					'value-type': 1,
+					'orderby': 'DATE',
+					'skip': 0,
+					'take': 8,
+				});
+				tempRecords[index].then(result => {
+					// window.console.log(result);
+					result.forEach(ele => {
+						tempArrX.push(ele.date)
+						tempArrY.push(ele.value)
+						// if(ele.valueType == 1) {
+						// 	tempObj.sales = ele.value;
+						// } else if(ele.valueType == 2) {
+						// 	tempObj.growth = ele.value;
+						// } else if(ele.valueType == 3) {
+						// 	tempObj.marketShare = ele.value;
+						// } else if(ele.valueType == 4) {
+						// 	tempObj.ms = ele.value;
+						// } else if(ele.valueType == 5) {
+						// 	tempObj.ei = ele.value;
+						// }
+					})
+					tempObj.date = tempArrX;
+					tempObj.data = tempArrY;
+					finalArr.pushObject(tempObj);
+					if(finalArr.length == arr.length) {
+						// window.console.log(finalArr);
+						this.set('overallLineData', finalArr);
+					}
 				})
 			})
 		})
@@ -185,8 +253,8 @@ export default Controller.extend({
 				'2018-09', '2018-10', '2018-11', '2018-12'],
 			data: [820, 932, 901, 934, 1290, 1330, 1320, 244, 365, 109, 203, 273]
 		}]));
-		// this.set('lineColor',  A(['rgb(115,171,255)', 'rgb(255,227,128)', 'rgb(73,229,245)','rgb(52,246,188)', 'rgb(54,179,126)']));
-		this.set('legendPosition', { x: 'center', y: 'center', });
+		this.set('linelegendPosition', { right: '0', 'top': '60', type: 'scoll', orient: 'vertical' });
+		this.set('grid', { right: '200px', });
 	},
 
 	
