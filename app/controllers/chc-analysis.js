ef71a8aaec343a44cf25994345a4ca6a32e65494
Chenghao Sun <601330984@qq.com>
2019-05-22 19:35:23 +0800

    表格组件修改 拼完一个chc表格

--- app/controllers/chc-analysis.js
+++ app/controllers/chc-analysis.js
@@ -6,15 +6,21 @@ export default Controller.extend({
     cur_tab_idx: 0,
 	tabs: A(['Overall Market', 'City Analysis']),
 	collapsed: false,
+
+	tableTake: 10,
 	
 	curInfoId: '',
 	curMarket: null,
 	curCity: null,
 	curDate: null,
 	curDateType: 4,
+
+	dateDataStateFlag: false,
 	
 	xAxisDataHeader: A([]),
 	doubleData: A([]),
+	productTableListData: A([]),
+	totalProObj: null,
 
 	overallInfo: computed('curMarket', function() {
 		let result = this.store.queryRecord('overallInfo', {'market-id': this.curMarket.id, 'orderby': ''});
@@ -39,6 +45,7 @@ export default Controller.extend({
 		});
 		result.then(res => {
 			this.set('curDate', res.firstObject);
+			this.set('dateDataStateFlag', true);
 		})
         return result;
 	}),
@@ -54,27 +61,99 @@ export default Controller.extend({
         return result;
 	}),
 	
-	mktSize: computed('curInfoId', 'cur_tab_idx', function() {
+	mktSize: computed('curInfoId', 'cur_tab_idx', 'curDate', 'dateDataStateFlag', function() {
+		if (this.curDate == undefined || this.curDate == null) {
+			return;
+		} 
 		let result = this.store.query('salesRecord', {
 			'info-id': this.curInfoId, 
-			'date': '2018Q4',
-			'date-type': 2,
+			'date': this.curDate.date,
+			'date-type': this.curDateType,
 			'address-type': this.cur_tab_idx,
 			'goods-type': 0,
 			'value-type': 1,
 		});
-        return result;
+		return result;
 	}),
 
-	mktGrowth: computed('curInfoId', 'cur_tab_idx', function() {
+	mktGrowth: computed('curInfoId', 'cur_tab_idx', 'curDate', 'dateDataStateFlag', function() {
+		if (this.curDate == undefined || this.curDate == null) {
+			return;
+		} 
 		let result = this.store.query('salesRecord', {
 			'info-id': this.curInfoId, 
-			'date': '2018Q4',
-			'date-type': 2,
+			'date': this.curDate.date,
+			'date-type': this.curDateType,
 			'address-type': this.cur_tab_idx,
 			'goods-type': 0,
 			'value-type': 2,
 		});
+        return result;
+	}),
+
+	productTableList: computed('curInfoId', 'cur_tab_idx', 'curDate', 'dateDataStateFlag', function() {
+		if (this.curDate == undefined || this.curDate == null) {
+			return;
+		} 
+		let result = this.store.query('salesRecord', {
+			'info-id': this.curInfoId, 
+			'date': this.curDate.date,
+			'date-type': this.curDateType,
+			'address-type': this.cur_tab_idx,
+			'goods-type': 1,
+			'value-type': 1,
+			'orderby': '-VALUE',
+			'skip': 0,
+			'take': this.tableTake,
+		});
+		result.then(res => {
+			let tempRecords = A([]);
+			let finalArr = A([]);
+			res.forEach((elem, index) => {
+				let tempObj = {};
+				tempObj.brand = elem.product.get('title');
+				tempRecords[index] = this.store.query('salesRecord', {
+					'info-id': this.curInfoId, 
+					'date': this.curDate.date,
+					'date-type': this.curDateType,
+					'address-type': this.cur_tab_idx,
+					'goods-type': 1,
+					'goods-id': elem.goodsId,
+					'orderby': 'VALUE_TYPE',
+					'skip': 0,
+				});
+				tempRecords[index].then(result => {
+					// window.console.log(result);
+					result.forEach(ele => {
+						if(ele.valueType == 1) {
+							tempObj.sales = ele.value;
+						} else if(ele.valueType == 2) {
+							tempObj.growth = ele.value;
+						} else if(ele.valueType == 3) {
+							tempObj.marketShare = ele.value;
+						} else if(ele.valueType == 4) {
+							tempObj.ms = ele.value;
+						} else if(ele.valueType == 5) {
+							tempObj.ei = ele.value;
+						}
+					})
+					finalArr.pushObject(tempObj)
+					// window.console.log(finalArr);
+					this.set('productTableListData', finalArr);
+					let totalPro = finalArr.reduce((total, ele) =>  {
+						return {
+							totalSales: Number(total.totalSales)+Number(ele.sales),
+							totalMarketShare:  Number(total.totalMarketShare)+ Number(ele.marketShare),
+							totalMs: total.totalMs+ele.ms,
+							totalGrowth: total.totalGrowth+ele.growth,
+							totalEi: total.totalEi+ele.ei
+						}
+					}, {totalSales:0, totalMarketShare: 0, totalMs: 0, totalGrowth: 0, totalEi: 0});
+					// console.log(totalPro)
+					this.set('totalProObj', totalPro)
+				})
+			})
+		})
         return result;
 	}),
 
