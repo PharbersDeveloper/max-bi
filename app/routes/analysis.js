12360c018cad67c2a48486e140c38c830bb56c1b
Frank <wsgoodman@163.com>
2019-05-23 17:34:38 +0800

    left menu 完成。

--- app/routes/analysis.js
+++ app/routes/analysis.js
@@ -4,17 +4,81 @@ import { A } from '@ember/array';
 
 export default Route.extend({
 	model() {
+		const store = this.store;
+
 		let markets = A([]),
 			firstMarketName = '',
+			regionsData = A([]),
+			provincesData = A([]),
+			citiesData = A([]),
+			provinceMapData = A([]),
+			provinceMapMaxValue = 0,
 			provinceProductPerformanceData = A([]),
-			provinceCompetitiveLnadscape = A([]);
+			provinceCompetitiveLnadscape = A([]),
+			yearMonths = A([]),
+			currentYear = new Date().getFullYear(),
+			settingEarliestYear = 2017,
+			defaultYearMonth = '';
 
-		return this.store.query('market', { 'company-id': '5ca069e2eeefcc012918ec73' })
+		// 生成时间区间
+		for (let i = settingEarliestYear; i < currentYear; i++) {
+			for (let j = 1; j <= 12; j++) {
+				let month = j < 10 ? `0${j}` : String(j);
+				yearMonths.unshift(i + month);
+			}
+		}
+		defaultYearMonth = yearMonths.firstObject;
+
+		return store.query('market', {
+			'company-id': '5ca069e2eeefcc012918ec73'
+		})
 			.then(data => {
 				markets = data;
 				firstMarketName = markets.firstObject.market;
+				return store.findAll('region');
+			})
+			.then(data => {
+				regionsData = data;
 
-				this.store.query('productaggregation', { 'company_id': '5ca069e2eeefcc012918ec73', 'market': 'ONC_other', 'orderby': '-SALES', 'take': '10', 'skip': '0', 'ym': '201802', 'ym_type': 'MAT', 'address': '上海市' })
+				return store.findAll('province');
+			})
+			.then(data => {
+				provincesData = data;
+
+				return store.findAll('city');
+			})
+			.then(data => {
+				citiesData = data;
+
+				return this.store.query('marketaggregation', {
+					'company_id': '5ca069e2eeefcc012918ec73',
+					market: firstMarketName,
+					orderby: '-SALES',
+					ym: yearMonths.firstObject,
+					'ym_type': 'YTD',
+					'address_type': 'PROVINCE'
+				})
+			})
+			.then(data => {
+				provinceMapData = data.map(ele => {
+					return {
+						name: ele.address,
+						value: ele.sales,
+						productCount: ele.productCount,
+						salesSom: ele.salesSom,
+						salesYearGrowth: ele.salesYearGrowth,
+						salesEi: ele.salesEi,
+						salesSomYearGrowth: ele.salesSomYearGrowth,
+					}
+				});
+				provinceMapMaxValue = Math.ceil(provinceMapData.sortBy('value').lastObject.value);
+
+				provinceMapData.push({ name: '台湾', value: 0 });
+				provinceMapData.push({ name: '香港', value: 0 });
+				provinceMapData.push({ name: '澳门', value: 0 });
+
+				console.log(provinceMapData);
+				console.log(provinceMapMaxValue);
 
 				return this.store.query('marketaggregation', {
 					'company_id': '5ca069e2eeefcc012918ec73',
@@ -22,24 +86,12 @@ export default Route.extend({
 					orderby: '-SALES',
 					take: '10',
 					skip: '0',
-					ym: '201802',
-					'ym_type': 'MAT',
-					'address_type': 'CITY'
+					ym: yearMonths.firstObject,
+					'ym_type': 'YTD',
+					'address_type': 'PROVINCE'
 				})
 			})
 			.then(data => {
-				// companyId: DS.attr('string'),
-				// market: DS.attr('string'),
-				// address: DS.attr('string'),
-				// addressType: DS.attr('string'),
-				// ym: DS.attr('number'),
-				// ymType: DS.attr('string'),
-				// sales: DS.attr('number'),
-				// productCount: DS.attr('number'),
-				// salesSom: DS.attr('number'),
-				// salesYearGrowth: DS.attr('number'),
-				// salesSomYearGrowth: DS.attr('number'),
-				// salesEI: DS.attr('number'),
 				provinceProductPerformanceData = data.map(ele => {
 					let marSize = ele.sales / ele.salesSom;
 					return {
@@ -57,13 +109,13 @@ export default Route.extend({
 					'company_id': '5ca069e2eeefcc012918ec73',
 					market: firstMarketName,
 					orderby: 'SALES_RANK',
-					'gte[ym]': 201801,
-					'lte[ym]': 201812,
-					'ym_type': 'MAT',
-					address: '北京市',
-					'address_type': 'CITY',
+					'gte[ym]': '201801',
+					'lte[ym]': '201812',
+					'ym_type': 'YTD',
+					address: provincesData.firstObject.title,
+					'address_type': 'PROVINCE',
 					'lte[sales_rank]': 10,
-					'current[ym]': 201801
+					'current[ym]': yearMonths.firstObject
 				})
 			})
 			.then(data => {
@@ -76,13 +128,19 @@ export default Route.extend({
 						name: ele.productName,
 						date: currentProductData.map(ele => ele.ym),
 						data: currentProductData.map(ele => ele.sales),
-
 					}
 				});
 			})
 			.then(() => {
 				return RSVP.hash({
+					yearMonths,
+					defaultYearMonth,
 					markets,
+					regionsData,
+					provincesData,
+					citiesData,
+					provinceMapData,
+					provinceMapMaxValue,
 					provinceProductPerformanceData,
 					provinceCompetitiveLnadscape
 				});
@@ -98,13 +156,28 @@ export default Route.extend({
 				market: item.market,
 			}
 			marketArr.push(market)
-		})
-		// controller.set('ymValue', arrval.firstObject.ym);
-		// controller.set('timeArr', arrval);
-		// controller.set('marketsArr', markets);
+		});
+		// 设置 市场选择 datum / 默认市场
 		controller.set('marketArr', marketArr);
 		controller.set('marketValue', marketArr.firstObject);
-		controller.set('files', model.provinceProductPerformanceData)
+		// 设置 区域选择 datum / 默认区域
+		controller.set('regionsData', model.regionsData);
+		controller.set('defaultRegion', model.regionsData.firstObject);
+		// 设置 省份选择 datum / 默认省份
+		controller.set('provincesData', model.provincesData);
+		controller.set('defaultProvince', model.provincesData.firstObject);
+		// 设置 城市选择 datum / 默认城市
+		controller.set('citiesData', model.citiesData);
+		controller.set('defaultCity', model.citiesData.firstObject);
+		// 设置 日期选择 datum / 默认日期 / 默认时间区间模式 
+		controller.set('timeArr', model.yearMonths);
+		controller.set('defaultYearMonth', model.defaultYearMonth);
+		controller.set('buttonGroupValue', 1);
+		// 设置 province 的 地图 / 表格 / 折线图
+		controller.set('provinceMapData', model.provinceMapData);
+		controller.set('provinceMapMaxValue', model.provinceMapMaxValue);
+
+		controller.set('provinceProductPerformanceData', model.provinceProductPerformanceData)
 		controller.set('provinceLineData', model.provinceCompetitiveLnadscape)
 
 	}
